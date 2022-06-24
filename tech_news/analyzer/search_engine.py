b34cadc5230532785870879672f481efb68d5a24
Gustavo <gus.inacio@hotmail.com>
2022-06-24 18:24:32 -0300

    feat: Search news by title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,17 @@
+import re
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = search_news({"title": re.compile(title, re.IGNORECASE)})
+    formatted_news = []
+
+    for item in news:
+        tuple_news = (item["title"], item["url"])
+        formatted_news.append(tuple_news)
+
+    return formatted_news
 
 
 # Requisito 7
